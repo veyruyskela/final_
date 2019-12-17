67b32c35c19eb2a144ccce9ffcfee486e0d5107f
emirrahim <rahimmr011@gmail.com>
2019-12-17 12:16:44 -0800

    media added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import Footer from "./components/Home Page/Footer";
-import './components/Home Page/Footer/index.css'
+import './components/Home Page/Footer/reset.scss'
+import './components/Home Page/Footer/index.scss'
 
 function App() {
-    const pList = [['CONTACT', 'HOW TO PURCHASE', 'PAYMENT'], ['Facebook', 'instagram', "Pinterest"], ['History', 'Policy', "Work with US"]];
+    const elements = [{
+        class_name: "About-us",
+        list : [
+            'Contact', 'How to Purchase', 'Payment'
+        ],
+        header: "About"
+    }, {
+        class_name: "Follow-us",
+        list : [
+            'Facebook', 'Instagram', 'Pinterest'
+        ],
+        header: "Follow"
+    } , {
+        class_name: "Help",
+        list : [
+            'History', 'Policy', "Work with Us"
+        ],
+        header: "Help"
+    }];
+
+    // const pList = [['CONTACT', 'HOW TO PURCHASE', 'PAYMENT'], ['Facebook', 'instagram', "Pinterest"], ['History', 'Policy', "Work with US"]];
+
     return (
-        <div className="footer">
-            {
-                pList.map((p) => {
-                    console.log(p);
-                    return <Footer
-                        listOfparagraphs={p}
-                    />
-                })
-            }
+    <div>
+        <Footer
+            all={elements}
+            class_name="footer"
+        />
         </div>
     )
 }
--- src/components/Home Page/Footer/index.js
+++ src/components/Home Page/Footer/index.js
@@ -4,25 +4,50 @@ import React from "react";
 
 class Footer extends React.Component{
     render() {
-        const {listOfparagraphs , class_name} = this.props;
+        const {all ,/*listOfparagraphs , */class_name} = this.props;
+        // return(
+        //     <div className={class_name}>
+        //         <h2>HELP</h2>
+        //         {/*<p>CONTACT</p>*/}
+        //         {/*<p>HOW TO PURCHASE</p>*/}
+        //         {/*<p>PAYMENT</p>*/}
+        //         {/*<p>{listOfparagraphs}</p>*/}
+        //         {
+        //             console.log(listOfparagraphs)
+        //         }
+        //         {
+        //             listOfparagraphs.map((tag, index)=>{ //niye forEach islemedi? //hemcinin index olmayanda niye xeta veriR?
+        //                 // console.log(tag);
+        //                     return <p key={index} className="footer-p">{tag}</p>
+        //             })
+        //         }
+        //     </div>
+        // )
+
         return(
-            <div className={class_name}>
-                <h2>HELP</h2>
-                {/*<p>CONTACT</p>*/}
-                {/*<p>HOW TO PURCHASE</p>*/}
-                {/*<p>PAYMENT</p>*/}
-                {/*<p>{listOfparagraphs}</p>*/}
+            <div className="footer">
                 {
-                    console.log(listOfparagraphs)
-                }
-                {
-                    listOfparagraphs.map((tag, index)=>{ //niye forEach islemedi? //hemcinin index olmayanda niye xeta veriR?
-                        // console.log(tag);
-                            return <p key={index} className="footer-p">{tag}</p>
+                    all.map((item , index)=>{
+                        return (
+                            <div className="footer__item">
+                                <a href={"#"} className="footer__item-header" onClick={handler} key={index}>{item.class_name}</a>
+                                <ul className="footer__item-content "  key={index}>
+                                    {
+                                    item.list.map(listElem=>{
+                                        return <li className="footer__content-text">{listElem}</li>
+                                    })
+                                    }
+                                </ul>
+                            </div>
+                        )
                     })
                 }
             </div>
-        )
+        );
+        function handler(e){
+            console.log(e.target.parentNode.children[1]);
+            e.target.parentNode.children[1].classList.toggle("active");//setActive === "" ? "active" : "");
+        }
     }
 }
 
